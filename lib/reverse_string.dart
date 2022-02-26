26deee5718d74197b69a55bd160ad69162fc6b45
Ankit Gupta <ankitgupta_@outlook.com>
2022-02-26 12:04:37 +0530

    add different approach to first reverse

--- lib/reverse_string.dart
+++ lib/reverse_string.dart
@@ -1,6 +1,19 @@
+
+//  mth => 2
 String firstReverse(String str) {
   return str.split('').reversed.join('');
 }
+
+  // mth => 1
+  // String firstReverse(String str) {
+  //   // code goes here
+  //   final temp = <String>[];
+  //   for (int i = str.length - 1; i >= 0; i--) {
+  //     temp.add(str[i]);
+  //   }
+  //   final ans = temp.join('');
+  //   return ans;
+  // }
 
 void main() {
   print(firstReverse('ankit'));
