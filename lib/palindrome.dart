3d2536ae0bd5a41677616c540ea517eb1092cc2f
Ankit Gupta <ankitgupta_@outlook.com>
2022-03-01 23:54:07 +0530

    add alternative palindrome solution

--- lib/palindrome.dart
+++ lib/palindrome.dart
@@ -1,7 +1,22 @@
 bool isPalindrome(String str) {
-  final revStr = str.split('').reversed.join();
-  return revStr == str;
+  final strArray = str.split('');
+  late bool result;
+  for (int i = 0; i <= strArray.length - 1; i++) {
+    if (strArray[i] == strArray[strArray.length - i - 1]) {
+      result = true;
+      continue;
+    } else {
+      result = false;
+      break;
+    }
+  }
+  return result;
 }
+
+// bool isPalindrome(String str) {
+//   final revStr = str.split('').reversed.join();
+//   return revStr == str;
+// }
 
 void main() {
   print(isPalindrome('rtr'));
