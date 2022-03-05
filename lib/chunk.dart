76224cf84e3cb26612a8db0717fbf4b020e0cea1
Ankit Gupta <ankitgupta_@outlook.com>
2022-03-05 14:31:19 +0530

    update correct solution for chunked

--- lib/chunk.dart
+++ lib/chunk.dart
@@ -1,28 +1,53 @@
 List getChunks(List list, int size) {
-  List chunked = [];
-  int index = 0;
-  bool canFit;
+  final List<List> chunked = [];
 
-  if (list.length % size == 0) {
-    canFit = true;
-  } else {
-    canFit = false;
-  }
-
-  while (index < list.length) {
-    int? splitSize;
-    if (!canFit && index == list.length - 1) {
-      splitSize = null;
+  for (final element in list) {
+    if (chunked.isEmpty) {
+      chunked.add([element]);
     } else {
-      splitSize = index + size;
+      final last = chunked.last;
+      if (last.length < size) {
+        last.add(element);
+      } else {
+        chunked.add([element]);
+      }
     }
-    chunked.add(list.sublist(index, splitSize));
-    index = index + size;
   }
 
   return chunked;
 }
 
+/// THis needs to be fixed
+// List getChunks(List list, int size) {
+//   List chunked = [];
+//   int index = 0;
+//   bool canFit;
+
+//   if (list.length % size == 0) {
+//     canFit = true;
+//   } else {
+//     canFit = false;
+//   }
+
+//   while (index < list.length) {
+//     int? splitSize;
+//     if (!canFit && index == list.length - 1) {
+//       splitSize = null;
+//     } else {
+//       splitSize = index + size;
+//     }
+//     chunked.add(list.sublist(index, splitSize));
+//     index = index + size;
+//   }
+
+//   return chunked;
+// }
+
 void main() {
-  print(getChunks([1, 23, 3, 34, 5, 6, 7, 8, 9], 2));
+  print(getChunks([1, 23, 3, 34, 5, 6, 7, 8, 9, 0], 2));
+  print(getChunks([1, 23, 3, 34, 5, 6, 7, 8, 9], 5));
+  print(getChunks([1, 23, 3, 34, 5, 6, 7, 8, 9], 3));
+  print(getChunks([1, 23, 3, 34, 5, 6, 7, 8, 9], 8));
+  print(getChunks([1, 23, 3, 34, 5, 6, 7, 8, 9], 6));
+  print(getChunks([1, 23, 3, 34, 5, 6, 7, 8, 9], 9));
 }
