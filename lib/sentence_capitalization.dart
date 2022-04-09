9ff505cff2dac0bb72625cbddecd6acc323f1c4d
Ankit Gupta <ankitgupta_@outlook.com>
2022-04-09 18:17:49 +0530

    add second Solution for capitalization

--- lib/sentence_capitalization.dart
+++ lib/sentence_capitalization.dart
@@ -1,15 +1,38 @@
 void main() {
+  print(capitalize('a a'));
   print(capitalize('a short sentence'));
   print(capitalize('a lazy fox'));
   print(capitalize('look, it is working!'));
 }
 
-String capitalize(String str) {
-  final words = [];
+//// SOLUTION = 2
 
-  for (String data in str.split(' ')) {
-    words.add(data[0].toUpperCase() + data.substring(1));
+String capitalize(String str) {
+  String result = str[0].toUpperCase();
+
+  for (int i = 1; i < str.length; i++) {
+    if (str[i - 1] == '') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
   }
 
-  return words.join(' ');
+  return result;
 }
+
+
+
+
+//// SOLUTION 1
+
+// String capitalize(String str) {
+//   final words = [];
+
+//   for (String data in str.split(' ')) {
+//     print(data);
+//     words.add(data[0].toUpperCase() + data.substring(1));
+//   }
+
+//   return words.join(' ');
+// }
