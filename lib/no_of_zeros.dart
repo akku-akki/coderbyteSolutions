dc40a955383061f066daa50017039806cd55feda
Ankit Gupta <ankitgupta_@outlook.com>
2022-06-18 17:43:19 +0530

    add special condition to count zero

--- lib/no_of_zeros.dart
+++ lib/no_of_zeros.dart
@@ -1,5 +1,6 @@
 void main() {
-  print(countZeros(10234));
+  // print(countZeros(000));
+  print(spCountZeros(1020));
 }
 
 int countZeros(int n, {int zeroCount = 0}) {
@@ -12,4 +13,17 @@ int countZeros(int n, {int zeroCount = 0}) {
   }
   int zeros = countZeros(n ~/ 10, zeroCount: zeroCount);
   return zeros;
+}
+
+/// SPecial condition how to pass a value to above calls
+
+int spCountZeros(int n, {int count = 0}) {
+  if (n == 0) {
+    return count;
+  }
+  int rem = n % 10;
+  if (rem == 0) {
+    return spCountZeros(n ~/ 10, count: count + 1);
+  }
+  return spCountZeros(n ~/ 10, count: count);
 }
