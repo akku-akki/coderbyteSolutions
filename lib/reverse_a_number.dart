4187b19cea9632203ce8173ee2a7df95d2051355
Ankit Gupta <ankitgupta_@outlook.com>
2022-06-18 16:58:57 +0530

    add second solution for reverse a digit

--- lib/reverse_a_number.dart
+++ lib/reverse_a_number.dart
@@ -1,7 +1,9 @@
+import 'dart:math' as Math;
+
 void main() {
-  reverseNumber1(
-    5837,
-  );
+  print(reverseNumberSol2(
+    1234,
+  ));
 }
 
 void reverseNumber1(int no, {int sum = 0}) {
@@ -13,4 +15,23 @@ void reverseNumber1(int no, {int sum = 0}) {
   final rem = no % 10;
   sum = sum * 10 + rem;
   reverseNumber1(no ~/ 10, sum: sum);
+}
+
+// double logBase(num x, num base) => log(x) / log(base);
+// double log10(num x) => log(x) / ln10;
+
+int reverseNumberSol2(int n) {
+  int digits = (Math.log(n) ~/ Math.ln10) + 1;
+  return reverseHelper(n, digits);
+}
+
+int reverseHelper(int n, int digit) {
+  if (n % 10 == n) {
+    return n;
+  }
+  final rem = n % 10;
+  final ans =
+      (rem * Math.pow(10, digit - 1) + reverseHelper(n ~/ 10, digit - 1))
+          .toInt();
+  return ans;
 }
