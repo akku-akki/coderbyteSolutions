49fdc78a8f558a1a87174e1cdab40600dfc17896
Ankit Gupta <ankitgupta_@outlook.com>
2022-07-04 21:09:22 +0530

    add solution for returning paths

--- lib/backtracking/maze_path.dart
+++ lib/backtracking/maze_path.dart
@@ -2,6 +2,7 @@
 
 void main() {
   getPath('', 3, 3);
+  print(retPaths('', 3, 3));
 }
 
 void getPath(String p, int r, int c) {
@@ -17,4 +18,23 @@ void getPath(String p, int r, int c) {
   if (c > 1) {
     getPath(p + 'R', r, c - 1);
   }
+}
+
+List<String> retPaths(String p, int r, int c) {
+  if (r == 1 && c == 1) {
+    List<String> path = []..add(p);
+    return path;
+  }
+
+  List<String> list = [];
+
+  if (r > 1) {
+    list.addAll(retPaths(p + 'D', r - 1, c));
+  }
+
+  if (c > 1) {
+    list.addAll(retPaths(p + 'R', r, c - 1));
+  }
+
+  return list;
 }
